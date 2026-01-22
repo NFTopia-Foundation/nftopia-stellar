b6dae429b5ceadf198f530e078aab3cdf2731a8c
Eniola3321 <ekanadeadeniyiabdulhafis@gmail.com>
2026-01-22 14:52:07 +0100

    Feat: create Rate-limit

--- nftopia-backend/src/app.module.ts
+++ nftopia-backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    {
+      ...ThrottlerModule.forRoot({
+        ttl: 60,
+        limit: 10,
+      }),
+    },
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
