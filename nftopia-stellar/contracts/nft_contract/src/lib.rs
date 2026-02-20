3ee51a2c9841fe730c33d5179e45dc342a947aa4
Your Name <you@example.com>
2026-02-20 12:47:05 +0100

    chore: Scaffold NFT contract project and Cargo.toml

--- nftopia-stellar/contracts/nft_contract/src/lib.rs
+++ nftopia-stellar/contracts/nft_contract/src/lib.rs
@@ -1,1 +1,17 @@
+#![no_std]
 
+pub mod access_control;
+pub mod error;
+pub mod events;
+pub mod interface;
+pub mod metadata;
+pub mod royalty;
+pub mod storage;
+pub mod token;
+pub mod transfer;
+pub mod types;
+
+#[cfg(test)]
+mod test;
+
+// Contract struct will be defined here later
